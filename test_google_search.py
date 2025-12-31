054bee63082f62d12df8039d35e05577af769572
Shahrukh Inamdar <inamdar.shahrukh.si@gmail.com>
2025-12-31 15:57:44 +0530

    test

--- test_google_search.py
+++ test_google_search.py
@@ -1,18 +1,42 @@
-import time
+# import time
+# from selenium import webdriver
+# from selenium.webdriver.chrome.options import Options
+# from selenium.webdriver.common import options
+
+# def test_google_search():
+#     # options = Options()
+#     # options.add_argument("--headless")
+#     # options.add_argument("--disable-gpu")
+
+#     # driver = webdriver.Chrome(options=options)
+#     driver = webdriver.Chrome()
+#     driver.maximize_window()
+#     driver.get("https://www.google.com")
+
+#     time.sleep(2)
+#     assert "Google" in driver.title
+#     driver.quit()
+
+
+import allure
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common import options
 
+
+@allure.title("Google Search Test")
+@allure.description("Verify Google homepage opens")
+@allure.severity(allure.severity_level.CRITICAL)
 def test_google_search():
-    # options = Options()
-    # options.add_argument("--headless")
-    # options.add_argument("--disable-gpu")
+    options = Options()
+    options.add_argument("--headless")
+    options.add_argument("--disable-gpu")
 
-    # driver = webdriver.Chrome(options=options)
-    driver = webdriver.Chrome()
-    driver.maximize_window()
-    driver.get("https://www.google.com")
+    driver = webdriver.Chrome(options=options)
 
-    time.sleep(2)
-    assert "Google" in driver.title
+    with allure.step("Open Google"):
+        driver.get("https://www.google.com")
+
+    with allure.step("Verify title"):
+        assert "Google" in driver.title
+
     driver.quit()
