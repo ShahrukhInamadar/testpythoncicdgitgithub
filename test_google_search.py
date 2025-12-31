86ad9fdfd7f154901715743785a7fd29f0abebdd
Shahrukh Inamdar <inamdar.shahrukh.si@gmail.com>
2025-12-31 15:22:38 +0530

    test

--- test_google_search.py
+++ test_google_search.py
@@ -1,12 +1,15 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common import options
 
 def test_google_search():
-    options = Options()
-    options.add_argument("--headless")
-    options.add_argument("--disable-gpu")
+    # options = Options()
+    # options.add_argument("--headless")
+    # options.add_argument("--disable-gpu")
 
-    driver = webdriver.Chrome(options=options)
+    # driver = webdriver.Chrome(options=options)
+    driver = webdriver.Chrome()
+    driver.maximize_window()
     driver.get("https://www.google.com")
 
     assert "Google" in driver.title
